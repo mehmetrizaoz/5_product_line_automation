4dedc08ac9762d27133e81f74b91ad6e746bbb5b
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-06-01 22:12:24 +0300

    number of employees per office query added

--- qt_app/form_queryresult.cpp
+++ qt_app/form_queryresult.cpp
@@ -12,13 +12,11 @@ Form_QueryResult::Form_QueryResult(QWidget *parent) :
     QGridLayout *layout = new QGridLayout();
     layout->addWidget(ui->tableWidget,0,0,1,0);
     layout->addWidget(ui->pushButton,1,0);
-    //layout->addWidget(ui->lineEdit,1,1);
     this->setLayout(layout);
     setWindowTitle(tr("List of all Customers"));
 
     this->setStyleSheet("background-color: rgb(123, 204, 181)");
     ui->tableWidget->setStyleSheet("background-color: white;");
-    //ui->lineEdit->setStyleSheet("background-color: white;");
 }
 
 Form_QueryResult::~Form_QueryResult()
@@ -28,13 +26,6 @@ Form_QueryResult::~Form_QueryResult()
 
 void Form_QueryResult::on_pushButton_clicked()
 {
-    //password = ui->lineEdit->text();
-
     database myDB = database();
-    //if(myDB.connect("127.0.0.1", "classicmodels", "root", password) != false){
-       myDB.execQuery("://queries/listCustomers", ui->tableWidget);
-    //}
-    //else{
-    //   qDebug() << "cant connect to database";
-    //}
+    myDB.execQuery("://queries/number_of_employees_for_each_office", ui->tableWidget);
 }
